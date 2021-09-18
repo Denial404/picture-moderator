7a492470b3e42db93ece081acfb542488bdc87cd
weelam <tao.lin6852@gmail.com>
2021-09-18 00:33:01 -0400

    FIRST COMMIT LOLOLOLOLOL :D

---
+++ main.py
@@ -0,0 +1,27 @@
+# Import module
+from nudenet import NudeDetector
+from nudenet import NudeClassifier
+from flask import Flask, request, jsonify
+import requests
+import os
+from dotenv import load_dotenv
+
+
+app = Flask(__name__)
+
+
+@app.route("/nude-net", methods=["GET"])
+def nudeNet(): 
+  image = "./jackblack.png"
+  detector = NudeDetector() # detector = NudeDetector('base') for the "base" version of detector.
+  detector = detector.detect(image)
+  classifier = NudeClassifier()
+  pokemon = classifier.classify(image)
+
+
+  return jsonify({"detector": detector, "classifier": pokemon})
+
+
+if __name__ == "__main__":
+  # initialize detector (downloads the checkpoint file automatically the first time)
+  app.run(debug=True)
