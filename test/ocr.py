9b32e55c6aacbdd9d1fc930b4e293ec91a207ab5
dannywuwu <d246wu@gmail.com>
2021-09-18 11:25:38 -0400

    ocr image path fix (moved into test, updated path)

--- test/ocr.py
+++ test/ocr.py
@@ -1,7 +1,7 @@
 import pytesseract
 import os
 
-img = 'cs.jpg'
+img ='./test/cs.jpg'
 # set tesseract path os var
 pytesseract.pytesseract.tesseract_cmd=r'{}'.format(os.environ['TESS_PATH'])
 text = pytesseract.image_to_string(img)
