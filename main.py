ea3ee8beef84a5a72209472bec064e6cafca1480
dannywuwu <d246wu@gmail.com>
2021-09-18 13:12:27 -0400

    4 space indentation for main

--- main.py
+++ main.py
@@ -7,21 +7,21 @@ from flask import Flask, request, jsonify
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
-def home(): 
-  return "what's good"
-  
+def home():
+    return "what's good"
+
 @app.route("/nude-net", methods=["GET"])
-def nudeNet(): 
-  image = "./test/jackblack.png"
-  detector = NudeDetector() # detector = NudeDetector('base') for the "base" version of detector.
-  detector = detector.detect(image)
-  classifier = NudeClassifier()
-  pokemon = classifier.classify(image)
+def nudeNet():
+    image = "./test/jackblack.png"
+    detector = NudeDetector() # detector = NudeDetector('base') for the "base" version of detector.
+    detector = detector.detect(image)
+    classifier = NudeClassifier()
+    pokemon = classifier.classify(image)
 
 
-  return jsonify({"detector": detector, "classifier": pokemon})
+    return jsonify({"detector": detector, "classifier": pokemon})
 
 
 if __name__ == "__main__":
-  # initialize detector (downloads the checkpoint file automatically the first time)
-  app.run(port=8080, debug=True)
+    # initialize detector (downloads the checkpoint file automatically the first time)
+    app.run(port=8080, debug=True)
