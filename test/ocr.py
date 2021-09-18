e006b334410e9be27e986aaab84de10cae0d946c
dannywuwu <d246wu@gmail.com>
2021-09-18 12:17:18 -0400

    more ocr test code - image to data

--- test/ocr.py
+++ test/ocr.py
@@ -1,3 +1,7 @@
+try:
+    from PIL import Image
+except ImportError:
+    import Image
 import pytesseract
 import os
 
@@ -7,3 +11,7 @@ pytesseract.pytesseract.tesseract_cmd=r'{}'.format(os.environ['TESS_PATH'])
 text = pytesseract.image_to_string(img)
 print(text)
 
+# print(pytesseract.image_to_boxes(Image.open(img)))
+
+# Get verbose data including boxes, confidences, line and page numbers
+print(pytesseract.image_to_data(Image.open(img)))
