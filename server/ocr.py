1912f6359f6403f2c0961a64d7b6942b65ea695c
weelam <wl.lin@mail.utoronto.ca>
2021-09-18 16:25:42 -0400

    changed from py tessearct to google vision api

--- server/ocr.py
+++ server/ocr.py
@@ -1,17 +1,30 @@
-try:
-    from PIL import Image
-except ImportError:
-    import Image
-import pytesseract
-import os
 
-img ='./test/cs.jpg'
-# set tesseract path os var
-pytesseract.pytesseract.tesseract_cmd=r'{}'.format(os.environ['TESS_PATH'])
-text = pytesseract.image_to_string(img)
-print(text)
+def detect_text_uri(uri):
+    """Detects text in the file located in Google Cloud Storage or on the Web.
+    """
+    from google.cloud import vision
+    client = vision.ImageAnnotatorClient()
+    image = vision.Image()
+    image.source.image_uri = uri
 
-# print(pytesseract.image_to_boxes(Image.open(img)))
+    response = client.text_detection(image=image)
+    texts = response.text_annotations
+    print('Texts:')
 
-# Get verbose data including boxes, confidences, line and page numbers
-print(pytesseract.image_to_data(Image.open(img)))
+    for text in texts:
+        print('\n"{}"'.format(text.description))
+
+        vertices = (['({},{})'.format(vertex.x, vertex.y)
+                    for vertex in text.bounding_poly.vertices])
+
+        print('bounds: {}'.format(','.join(vertices)))
+
+    if response.error.message:
+        raise Exception(
+            '{}\nFor more info on error messages, check: '
+            'https://cloud.google.com/apis/design/errors'.format(
+                response.error.message))
+
+if __name__ == '__main__':
+    url = "https://cdn.vox-cdn.com/thumbor/cV8X8BZ-aGs8pv3D-sCMr5fQZyI=/1400x1400/filters:format(png)/cdn.vox-cdn.com/uploads/chorus_asset/file/19933026/image.png"
+    detect_text_uri(url)
