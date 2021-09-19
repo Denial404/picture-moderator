769e7c9dbd141bbc53080e658871c35b95f604ec
RichardYSun <richard-y-sun@hotmail.com>
2021-09-19 02:02:36 -0400

    Adds image censoring using sfw image

--- server/censoring.py
+++ server/censoring.py
@@ -2,43 +2,45 @@ from PIL import Image, ImageDraw
 from nudenet import NudeDetector
 # from nudenet import NudeClassifier
 
-class NudeArea:
+class NsfwArea:
     def __init__(self, bounds, label, score):
-        self.y_min = bounds[1]
-        self.x_min = bounds[0]
-        self.y_max = bounds[3]
-        self.x_max = bounds[2]
+        self.y_min = bounds[0]
+        self.x_min = bounds[1]
+        self.y_max = bounds[2]
+        self.x_max = bounds[3]
         self.label = label
         self.score = score
 
-def getNudeAreas(nudeResults):
-    nudeAreas = []
-    for nudeResult in nudeResults:
-        nudeAreas.append(NudeArea(nudeResult["box"],
-                                  nudeResult["label"],
-                                  nudeResult["score"]))
+def getNsfwAreas(nudeResults):
+    nsfwAreas = []
+    for nsfwArea in nudeResults:
+        nsfwAreas.append(NsfwArea(nsfwArea["box"],
+                                  nsfwArea["label"],
+                                  nsfwArea["score"]))
 
-    return nudeAreas
+    return nsfwAreas
 
-def censorImage(detector, nsfwImagePath, sfwImagePath = ""):
-    nudeResults = detector.detect(nsfwImagePath)
-    #classifier = NudeClassifier()
-    #pokemon = classifier.classify(nsfwImagePath)
-
-    nudeAreas = getNudeAreas(nudeResults)
+def censorImage(results, nsfwImagePath, sfwImagePath = ""):
+    nsfwAreas = getNsfwAreas(results)
 
     with Image.open(nsfwImagePath) as img:
         draw = ImageDraw.Draw(img)
-        for nudeArea in nudeAreas:
+        for nsfwArea in nsfwAreas:
             if (sfwImagePath == ""):
-                draw.rectangle([nudeArea.x_min, nudeArea.y_min, nudeArea.x_max, nudeArea.y_max], '#0f0f0f80', '#0f0f0f80', 2)
-
+                draw.rectangle([nsfwArea.x_min, nsfwArea.y_min, nsfwArea.x_max, nsfwArea.y_max], '#0f0f0f80', '#0f0f0f80', 2)
             else:
-                print("hello")
+                sfwImage = Image.open(sfwImagePath)
+
+                size = nsfwArea.x_max - nsfwArea.x_min, nsfwArea.y_max - nsfwArea.y_min
+                sfwImage.resize(size)
+
+                offset = nsfwArea.x_min, nsfwArea.y_min
+
+                img.paste(sfwImage, offset)
 
     x = nsfwImagePath.split("/")
     del x[-1]
-    censoredImagePath = "sfw_" + nsfwImagePath
+    censoredImagePath = "/".join(x) + "/sfw_" + nsfwImagePath.split("/")[-1]
 
     img.save(censoredImagePath)
 
