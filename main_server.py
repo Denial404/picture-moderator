d1ae6f6d5bc7d76f657704f7d6f5c8c8ca277220
weelam <wl.lin@mail.utoronto.ca>
2021-09-19 05:27:00 -0400

    playing with unicorn seeing if it'l lwork

--- main_server.py
+++ main_server.py
@@ -43,10 +43,7 @@ def analyze_text():
     }
     return jsonify({'analysis': analysis})
 
-def server_start():
-    nltk.downloader.download('vader_lexicon')
-    nltk.downloader.download('stopwords')
-    app.run(debug=True)
 
 if __name__ == "__main__":
-    server_start()
+    # gunicorn will not access this
+    app.run(debug=True)
