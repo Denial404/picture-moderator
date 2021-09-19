e223d8cb762a97b2217e749e4eae5ebfd76d0aa5
weelam <wl.lin@mail.utoronto.ca>
2021-09-18 21:14:07 -0400

    added new service to app.yaml

--- main_bot.py
+++ main_bot.py
@@ -1,8 +1,9 @@
 import os
 import discord
 from discord.ext import commands
-from bot.bot_request import BotRequest
+from bot_request import BotRequest
 from dotenv import load_dotenv
+
 load_dotenv()
 
 client = commands.Bot(command_prefix='pp ', case_insensitive=True, help_command=None)
